7159fbe15d7e1eee0d530271e45de0e64cfbc020
Dave Monk <david__monk@outlook.com>
2021-10-29 15:37:51 +0100

    Upgraded Burger menu icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,6 +16,9 @@ export class Header extends Component {
           {/* <div className="bb-nav-link bouncy ripple" style={{animationDelay: "0.42s"}} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity"/></div> */}
         </div>
         <div className="burger-menu" >
+          <div class="stick stick-1"></div>
+          <div class="stick stick-2"></div>
+          <div class="stick stick-3"></div>
           <div className="bb-nav-links">
             <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
             <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
