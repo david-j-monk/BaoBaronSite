78f618095b80e718efdcdcf066ad206cfbaf2502
Andrew Forrest <andrew@elmstonesystems.co.uk>
2021-10-30 13:09:39 +0100

    header is now banner over menu, added some images

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ export class Header extends Component {
 
     return (
       <div className="bb-header">
+        <div className="bb-header-logo" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Home" /></div>
         <div className="bb-nav-links">
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
+          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="About Us" /></div>
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.14s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
-          <div className="ripple logo" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Home" /></div>
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Our Story" /></div>
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.35s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Vegan" /></div>
-          {/* <div className="bb-nav-link bouncy ripple" style={{animationDelay: "0.42s"}} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity"/></div> */}
+          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
+          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
+          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
         </div>
         <div className="burger-menu" >
           <div class="stick stick-1"></div>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,41 +4,44 @@ import Instapost from './Instapost'
 export class HomePage extends Component {
   render() {
     return (
-      <div className="bb-homepage">
-        Address:
-        <br />
-        <br />
-        The Bao Baron, <br />
-        <br />
-        Folkestone
-        <br />
-        CT20 1JR
-        <br />
-        <br />
-        <br />
-        Contact Info:
-        <br />
-        <br />
-        01303244666 / the.lastbaron@outlook.com
-        <br />
-        <br />
-        Social Media Links:
-        <br />
-        <br />
-        https://www.facebook.com/thebaobaron
-        <br />
-        https://www.instagram.com/thebaobaron/
-        <br />
-        <br />
-        Business hours:
-        <br />
-        Wed - Thurs 5pm - 7:30pm
-        <br />
-        Fri - Sat 12pm - 2pm / 5pm / 8.30pm
-        <br />
-        Open Sundays during school holidays and national lockdowns 12pm - 2pm /
-        5pm / 7:30pm <br />
-        <Instapost />
+      <div className="bb-homepage-layout"> 
+        <div className="bb-homepage-mainimage" />
+        <div className="bb-homepage-about">
+          Address: 
+          <br />
+          <br />
+          The Bao Baron, <br />
+          <br />
+          Folkestone
+          <br />
+          CT20 1JR
+          <br />
+          <br />
+          <br />
+          Contact Info:
+          <br />
+          <br />
+          01303244666 / the.lastbaron@outlook.com
+          <br />
+          <br />
+          Social Media Links:
+          <br />
+          <br />
+          https://www.facebook.com/thebaobaron
+          <br />
+          https://www.instagram.com/thebaobaron/
+          <br />
+          <br />
+          Business hours:
+          <br />
+          Wed - Thurs 5pm - 7:30pm
+          <br />
+          Fri - Sat 12pm - 2pm / 5pm / 8.30pm
+          <br />
+          Open Sundays during school holidays and national lockdowns 12pm - 2pm /
+          5pm / 7:30pm <br />
+          <Instapost />
+        </div>
       </div>
     );
   }
