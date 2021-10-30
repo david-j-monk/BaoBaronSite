827ed4bf86d93d18e38976119a1f49048ebfe169
Andrew Forrest <andrew@elmstonesystems.co.uk>
2021-10-30 15:47:31 +0100

    fixed animation on hamburger menu, fixed header height issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import { NavItem } from "./NavItem";
 
 export class Header extends Component {
+  
+  state = { isActive: false };
+  handleToggle = () => {
+    this.setState({ isActive: !this.state.isActive });  };
 
   render() {
-
+    const isActive = this.state.isActive;
     return (
       <div className="bb-header">
-        <div className="bb-header-logo" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Home" /></div>
+        <div id="bb-header-logo-pos">
+          <div className="bb-header-logo" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Home" /></div>
+        </div>
         <div className="bb-nav-links">
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="About Us" /></div>
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.14s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
@@ -15,10 +21,13 @@ export class Header extends Component {
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
         </div>
-        <div className="burger-menu" >
-          <div class="stick stick-1"></div>
-          <div class="stick stick-2"></div>
-          <div class="stick stick-3"></div>
+        <div className={`bb-main-menu ${isActive ? "danger" : ""}`} onClick={this.handleToggle}> 
+          <div id="bb-main-menu-burger" className={isActive ? "open" : null}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
           <div className="bb-nav-links">
             <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
             <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
