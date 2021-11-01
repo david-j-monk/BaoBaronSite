eff701ff36ac0e82dc1648cffd6b37272040851d
Dave Monk <david__monk@outlook.com>
2021-11-01 12:00:37 +0000

    Made burger menu work

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import { NavItem } from "./NavItem";
 
 export class Header extends Component {
-  
+
   state = { isActive: false };
   handleToggle = () => {
-    this.setState({ isActive: !this.state.isActive });  };
+    this.setState({ isActive: !this.state.isActive });
+  };
 
   render() {
     const isActive = this.state.isActive;
@@ -21,18 +22,19 @@ export class Header extends Component {
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.35s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
         </div>
-        <div className={`bb-main-menu ${isActive ? "danger" : ""}`} onClick={this.handleToggle}> 
+        <div className={`bb-main-menu ${isActive ? "danger" : ""}`} onClick={this.handleToggle}>
           <div id="bb-main-menu-burger" className={isActive ? "open" : null}>
             <span></span>
             <span></span>
             <span></span>
-            <span></span>
+            <span></span> 
           </div>
-          <div className="bb-nav-links">
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Our Story" /></div>
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Vegan" /></div>
+          <div className={`bb-nav-links ${isActive ? "open" : ""}`}>
+          <div className="bb-nav-link" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageScroller("Home", "about")}><NavItem name="About Us" /></div>
+          <div className="bb-nav-link" style={{ animationDelay: "0.14s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
+          <div className="bb-nav-link" style={{ animationDelay: "0.21s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
+          <div className="bb-nav-link" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
+          <div className="bb-nav-link" style={{ animationDelay: "0.35s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
           </div>
         </div>
       </div>
---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Menu extends Component {
+  render() {
+    return (
+      <div>
+        Map
+      </div>
+    )
+  }
+}
+
+export default Menu
