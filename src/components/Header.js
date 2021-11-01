706ccf0a249d031bf0758ee438b2b9a336b1f3fc
Dave Monk <david__monk@outlook.com>
2021-11-01 17:41:05 +0000

    Fixed routing in burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,17 +17,16 @@ export class Header extends Component {
       <div className="bb-header">
 
         <div id="bb-header-logo-pos">
-          <Link to="/">
-            <div className="bb-header-logo ripple" ><NavItem name="Home" /></div>
+          <Link to="/" className="bb-header-logo ripple" ><NavItem name="Home" />
           </Link>
         </div>
 
         <div className="bb-nav-links">
-          <Link to="/"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageScroller("about")}><NavItem name="About Us" /></div></Link>
-          <Link to="/Suppliers"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.14s" }}><NavItem name="Suppliers" /></div></Link>
-          <Link to="/Menu"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.21s" }}><NavItem name="Menu" /></div></Link>
-          <Link to="/Booking"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }}><NavItem name="Booking" /></div></Link>
-          <Link to="/Publicity"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.35s" }}><NavItem name="Publicity" /></div></Link>
+          <Link to="/" className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageScroller("about")}><NavItem name="About Us" /></Link>
+          <Link to="/Suppliers" className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.14s" }}><NavItem name="Suppliers" /></Link>
+          <Link to="/Menu" className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.21s" }}><NavItem name="Menu" /></Link>
+          <Link to="/Booking" className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }}><NavItem name="Booking" /></Link>
+          <Link to="/Publicity" className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.35s" }}><NavItem name="Publicity" /></Link>
         </div>
         <div className={`bb-main-menu ${isActive ? "open" : ""}`} onClick={this.handleToggle}>
           <div id="bb-main-menu-burger" className={isActive ? "open" : null}>
@@ -37,11 +36,11 @@ export class Header extends Component {
             <span></span>
           </div>
           <div className={`bb-nav-links ${isActive ? "open" : ""}`}>
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
-            <div className="bb-nav-link" onClick={e => this.props.pageScroller("Home", "about")}><NavItem name="About Us" /></div>
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
-            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
+            <Link to="/Menu" className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></Link>
+            <Link to="/Booking" className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></Link>
+            <Link to="/" className="bb-nav-link" onClick={e => this.props.pageScroller("about")}><NavItem name="About Us" /></Link>
+            <Link to="/Suppliers" className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></Link>
+            <Link to="/Publicity" className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></Link>
           </div>
         </div>
       </div>
