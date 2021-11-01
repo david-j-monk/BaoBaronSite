706dd571cd8d26f9396da77ccb2f39deaaccfce3
Dave Monk <david__monk@outlook.com>
2021-11-01 17:12:20 +0000

    Added routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { NavItem } from "./NavItem";
+import {
+  Link
+} from "react-router-dom";
 
 export class Header extends Component {
 
@@ -12,29 +15,33 @@ export class Header extends Component {
     const isActive = this.state.isActive;
     return (
       <div className="bb-header">
+
         <div id="bb-header-logo-pos">
-          <div className="bb-header-logo ripple" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Home" /></div>
+          <Link to="/">
+            <div className="bb-header-logo ripple" ><NavItem name="Home" /></div>
+          </Link>
         </div>
+
         <div className="bb-nav-links">
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageScroller("Home", "about")}><NavItem name="About Us" /></div>
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.14s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.21s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
-          <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.35s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
+          <Link to="/"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageScroller("about")}><NavItem name="About Us" /></div></Link>
+          <Link to="/Suppliers"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.14s" }}><NavItem name="Suppliers" /></div></Link>
+          <Link to="/Menu"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.21s" }}><NavItem name="Menu" /></div></Link>
+          <Link to="/Booking"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.28s" }}><NavItem name="Booking" /></div></Link>
+          <Link to="/Publicity"><div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.35s" }}><NavItem name="Publicity" /></div></Link>
         </div>
         <div className={`bb-main-menu ${isActive ? "danger" : ""}`} onClick={this.handleToggle}>
           <div id="bb-main-menu-burger" className={isActive ? "open" : null}>
             <span></span>
             <span></span>
             <span></span>
-            <span></span> 
+            <span></span>
           </div>
           <div className={`bb-nav-links ${isActive ? "open" : ""}`}>
-          <div className="bb-nav-link" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageScroller("Home", "about")}><NavItem name="About Us" /></div>
-          <div className="bb-nav-link" style={{ animationDelay: "0.14s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
-          <div className="bb-nav-link" style={{ animationDelay: "0.21s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
-          <div className="bb-nav-link" style={{ animationDelay: "0.28s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
-          <div className="bb-nav-link" style={{ animationDelay: "0.35s" }} onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
+            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Menu" /></div>
+            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Booking" /></div>
+            <div className="bb-nav-link" onClick={e => this.props.pageScroller("Home", "about")}><NavItem name="About Us" /></div>
+            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Suppliers" /></div>
+            <div className="bb-nav-link" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Publicity" /></div>
           </div>
         </div>
       </div>
--- src/components/PageHandler.js
+++ src/components/PageHandler.js
@@ -7,45 +7,58 @@ import Publicity from "./Publicity";
 import Suppliers from "./Suppliers";
 import Booking from "./Booking";
 import Footer from "./Footer";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export class PageHandler extends Component {
-  state = { currentPage: "Home" };
-  renderSwitch(param) {
-    switch (param) {
-      case "Home":
-        return <HomePage />;
-      case "Suppliers":
-        return <Suppliers />;
-      case "Menu":
-        return <Menu />;
-      case "Vegan":
-        return <Vegan />;
-      case "Publicity":
-        return <Publicity />;
-        case "Booking":
-          return <Booking />;
-      default:
-        return <HomePage />;
-    }
+
+  renderSwitch() {
+
+    return <Switch>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="/Suppliers">
+        <Suppliers />
+      </Route>
+      <Route path="/Menu">
+        <Menu />
+      </Route>
+      <Route path="/Vegan">
+        <Vegan />
+      </Route>
+      <Route path="/Publicity">
+        <Publicity />
+      </Route>
+      <Route path="/Booking">
+        <Booking />
+      </Route>
+    </Switch>
+
   }
 
   pageSwitcher = (target) => {
     this.setState({ currentPage: target });
   }
 
-  pageScroller = (page, target) => { 
-    this.setState({ currentPage: page });
+  pageScroller = (target) => {
     var el = document.getElementById(target);
     if (el) el.scrollIntoView();
   }
 
   render() {
     return (
-      <div className="bb-page">
-        <Header pageSwitcher={this.pageSwitcher} pageScroller={this.pageScroller} />
-        <div className="bb-body">{this.renderSwitch(this.state.currentPage)}</div>
-        <Footer />
-      </div>
+      <Router>
+
+        <div className="bb-page">
+          <Header pageSwitcher={this.pageSwitcher} pageScroller={this.pageScroller} />
+          <div className="bb-body">{this.renderSwitch()}</div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
