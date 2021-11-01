66e45bf560c7c6265c038f22e90228916e6d92e4
Dave Monk <david__monk@outlook.com>
2021-11-01 13:51:01 +0000

    Fixed Footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export class Header extends Component {
     return (
       <div className="bb-header">
         <div id="bb-header-logo-pos">
-          <div className="bb-header-logo" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Home" /></div>
+          <div className="bb-header-logo ripple" onClick={e => this.props.pageSwitcher(e.target.innerText)}><NavItem name="Home" /></div>
         </div>
         <div className="bb-nav-links">
           <div className="bb-nav-link bouncy ripple" style={{ animationDelay: "0.07s" }} onClick={e => this.props.pageScroller("Home", "about")}><NavItem name="About Us" /></div>
