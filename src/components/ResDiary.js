cf350ad98bd6baa16eca21c2d19396df292ef11b
David Monk <33057365+david-j-monk@users.noreply.github.com>
2021-11-18 15:53:38 +0000

    Added Resdiary

--- src/components/ResDiary.js
+++ src/components/ResDiary.js
@@ -4,7 +4,10 @@ export class Menu extends Component {
   render() {
     return (
       <div>
-        ResDiary
+        <div id="rd-widget-frame" style="max-width: 600px; margin: auto;"></div>
+
+<input id="rdwidgeturl" name="rdwidgeturl" value="https://booking.resdiary.com/widget/Standard/TheBaoBaron/30129?includeJquery=false" type="hidden">
+<script type="text/javascript" src="https://booking.resdiary.com/bundles/WidgetV2Loader.js"></script>
       </div>
     )
   }
